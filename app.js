f0208c0fc961b902053b09b3cd67758c98f07931
yrhnm <38804445+yrhnm@users.noreply.github.com>
2018-04-30 02:42:02 +0900

    ミスの修正

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ function fib(n) {
     if (memo.has(n)) {
         return memo.get(n);
     } 
-    const value = fi(n - 1) + fib(n -2);
+    const value = fib(n - 1) + fib(n -2);
     memo.set(n, value);
     return value;
 }
